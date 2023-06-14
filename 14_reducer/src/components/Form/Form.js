7cbf226cbdd00ae12c5b8a855f32a29af6c73231
gvnadir <gianlucanadir@gmail.com>
2023-06-14 12:41:28 +0200

    from state to reducer

---
+++ 14_reducer/src/components/Form/Form.js
@@ -0,0 +1,88 @@
+import { useReducer } from "react";
+import classes from "./Form.module.css";
+
+const initialFormState = {
+  email: {
+    value: "",
+    isValid: false,
+  },
+  password: {
+    value: "",
+    isValid: false,
+  },
+};
+
+function formReducer(state, action) {
+  if (action.type === "EMAIL_CHANGE") {
+    return {
+      ...state,
+      email: {
+        value: action.payload,
+        isValid: action.payload.includes("@"),
+      },
+    };
+  }
+  if (action.type === "PASSWORD_CHANGE") {
+    return {
+      ...state,
+      password: {
+        value: action.payload,
+        isValid: action.payload.trim().length > 7,
+      },
+    };
+  }
+
+  return initialFormState;
+}
+
+function Form() {
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState(false);
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  // const [passwordIsValid, setPasswordIsValid] = useState(false);
+  const [formState, dispatch] = useReducer(formReducer, initialFormState);
+
+  const formIsValid = formState.email.isValid && formState.password.isValid;
+
+  function changeEmailHandler(event) {
+    const value = event.target.value;
+    dispatch({ type: "EMAIL_CHANGE", payload: value });
+    // setEnteredEmail(value);
+    // setEmailIsValid(value.includes("@"));
+  }
+
+  function changePasswordHandler(event) {
+    const value = event.target.value;
+    dispatch({ type: "PASSWORD_CHANGE", payload: value });
+    // setEnteredPassword(value);
+    // setPasswordIsValid(value.trim().length > 7);
+  }
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    if (!formIsValid) {
+      alert("Invalid form inputs!");
+      return;
+    }
+
+    console.log("Good job!");
+    console.log(formState.email.value, formState.password.value);
+  }
+
+  return (
+    <form className={classes.form} onSubmit={submitFormHandler}>
+      <div className={classes.control}>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" onChange={changeEmailHandler} />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" onChange={changePasswordHandler} />
+      </div>
+      <button>Submit</button>
+    </form>
+  );
+}
+
+export default Form;
